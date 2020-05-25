05d4ba0cea2892bbca61b4b32fb0a76af6501994
satyam9753 <satyam.anand9753@gmail.com>
2020-05-25 11:30:16 +0530

    additional files updated->0138

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='NNFL-EEG-DRCNN',
     version='1.11',
-    packages=['colab', '', '', ''],
+    packages=['colab', 'models', 'utilities', 'generate_images.py', 'train.py'],
 
-    install_requires=['numpy==1.13.1', 'scipy==0.19.1', 'scikit-learn==0.18.2', 'theano==0.8',
+    install_requires=['ipython==7.11.1', 'numpy==1.18.1', 'scipy==1.4.1', 'scikit-learn==0.21.3', 'theano==1.0.4',
                       'lasagne @ git+https://github.com/Lasagne/Lasagne.git#egg=lasagne=0.2.dev1'],
+    
     url='https://github.com/satyam9753/NNFL-EEG-DRCNN',
 
     license='GNU GENERAL PUBLIC LICENSE',
